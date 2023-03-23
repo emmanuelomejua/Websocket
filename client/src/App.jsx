823769a7147e5bc39c9a6d867204e17995fe6945
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-03-23 11:57:55 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './app.css'
+
+const App =() => {
+  return (
+    <main className='container'>
+      <div className='login'>
+        <input type='text' placeholder="Username"/>
+        <button>Login</button>
+      </div>
+      Socket
+    </main>
+  );
+}
+
+export default App;
